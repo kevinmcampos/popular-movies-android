df8bc464f04db78f02c3cb1119d7f3efdebcf8d1
Kevin Campos <kevinmcampos@gmail.com>
2017-07-22 21:23:56 -0300

    Create preferences screen with "Top rated" and "Most popular" options

--- app/src/main/java/me/kevincampos/popularmovies/ui/HomeActivity.java
+++ app/src/main/java/me/kevincampos/popularmovies/ui/HomeActivity.java
@@ -1,14 +1,18 @@
 package me.kevincampos.popularmovies.ui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import java.util.List;
 
@@ -35,7 +39,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
-        setActionBar(toolbar);
+        setSupportActionBar(toolbar);
         TextView toolbarText = (TextView) toolbar.getChildAt(0);
         toolbarText.setTextColor(Color.BLACK);
         toolbarText.setFontFeatureSettings("smcp");
@@ -63,6 +67,24 @@ public class HomeActivity extends AppCompatActivity {
         moviesGrid.setHasFixedSize(true);
 
         moviesDataManager.loadPage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_action:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void fixStatusBarPadding() {
